57b8b82bfd6a1239f1610d66fa8e304f655e809b
mhase <mhase1228@gmail.com>
2023-06-17 00:36:33 +0900

    format
    コードの整形を実施

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 # VPC
 resource "aws_vpc" "ex_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
 
   tags = {
@@ -10,9 +10,9 @@ resource "aws_vpc" "ex_vpc" {
 
 # パブリックサブネット
 resource "aws_subnet" "ex_subnet_a" {
-  vpc_id = aws_vpc.ex_vpc.id
-  cidr_block = "10.0.0.0/24"
-  availability_zone = "ap-northeast-1a"
+  vpc_id                  = aws_vpc.ex_vpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "ap-northeast-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -22,33 +22,33 @@ resource "aws_subnet" "ex_subnet_a" {
 
 # インターネットゲートウェイ
 resource "aws_internet_gateway" "ex_igw" {
-    vpc_id = aws_vpc.ex_vpc.id
+  vpc_id = aws_vpc.ex_vpc.id
 
-    tags = {
-      Name = "ex-igw"
-    }
+  tags = {
+    Name = "ex-igw"
+  }
 }
 
 # ルートテーブル
 resource "aws_route_table" "ex_route_table" {
-    vpc_id = aws_vpc.ex_vpc.id
+  vpc_id = aws_vpc.ex_vpc.id
 
-    tags = {
-      Name = "ex-route-table"
-    }
-  
+  tags = {
+    Name = "ex-route-table"
+  }
+
 }
 
 # ルート
 resource "aws_route" "ex_route_table" {
-  route_table_id = aws_route_table.ex_route_table.id
+  route_table_id         = aws_route_table.ex_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.ex_igw.id
+  gateway_id             = aws_internet_gateway.ex_igw.id
 }
 
 #ルートテーブルとの関連付け
 resource "aws_route_table_association" "ex_route_table_a" {
-    route_table_id = aws_route.ex_route_table.id
-    subnet_id = aws_subnet.ex_subnet_a.id
-  
+  route_table_id = aws_route.ex_route_table.id
+  subnet_id      = aws_subnet.ex_subnet_a.id
+
 }
